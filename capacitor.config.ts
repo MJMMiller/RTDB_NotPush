7118c2e4aba3c39f10aa68c52d9f53d101123f5c
MJMMiller <mateomiller1605@gmail.com>
2024-04-23 17:19:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RTDB_NotPush',
+  webDir: 'www'
+};
+
+export default config;
