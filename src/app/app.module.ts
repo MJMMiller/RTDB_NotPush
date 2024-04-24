1f29a1b3acdfbb4834eb56ce7c40cf431124bcc4
MJMMiller <mateomiller1605@gmail.com>
2024-04-23 19:41:02 -0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase} from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirebaseApp(() => initializeApp({"projectId":"rtdb---iot-7687c","appId":"1:522868287328:web:23f23f4b6c84828d4b6005","databaseURL":"https://rtdb---iot-7687c-default-rtdb.firebaseio.com","storageBucket":"rtdb---iot-7687c.appspot.com","apiKey":"AIzaSyDmFhG3Frq1_Bi9RPAVw0Ry5uNHqM47JE4","authDomain":"rtdb---iot-7687c.firebaseapp.com","messagingSenderId":"522868287328"})),],
+    
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { Database, object, ref } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  luminosidad: number = 0;
+  isDay: boolean = true;
 
-  constructor() {}
+  constructor(private database: Database) {}
+  
+  ngOnInit() {
+    this.accessFirebaseData();
+  }
 
+  accessFirebaseData() {
+    const luzRoute = ref(this.database, "/sensor/luz");
+    object(luzRoute).subscribe(async value => {
+      this.luminosidad = value.snapshot.val();
+      this.updateProgress(this.luminosidad);
+      await this.notifyDayOrNight();
+    });
+  }
+
+  updateProgress(value: number) {
+    const circularProgress = document.querySelector('.circular-progress') as HTMLElement;
+    const progressValue = document.querySelector('.progress-value') as HTMLElement;
+
+    progressValue.textContent = `${value}%`;
+    circularProgress.style.background = `conic-gradient(#FFE503 ${value * 3.6}deg, #ededed 0deg)`;
+  }
+
+  async notifyDayOrNight() {
+    const threshold = 50; // Define un umbral de luminosidad para distinguir día y noche
+    if (this.luminosidad >= threshold && !this.isDay) {
+      this.isDay = true;
+      await this.sendLocalNotification("Es de día", "El nivel de luminosidad indica que ahora es de día.");
+    } else if (this.luminosidad < threshold && this.isDay) {
+      this.isDay = false;
+      await this.sendLocalNotification("Es de noche", "El nivel de luminosidad indica que ahora es de noche.");
+    }
+  }
+
+  async sendLocalNotification(title: string, body: string) {
+    await LocalNotifications.requestPermissions();
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: title,
+          body: body,
+          id: new Date().getTime() // Usar un ID único basado en la hora actual
+        }
+      ]
+    });
+  }
 }
